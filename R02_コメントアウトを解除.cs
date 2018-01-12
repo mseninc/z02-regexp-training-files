4cb1b77ada04b0b745768004a79584a018a87c0e
Kenji YAMADA <kenzauros@users.noreply.github.com>
2018-01-12 22:59:50 +0900

    first commit

---
+++ R02_コメントアウトを解除.cs
@@ -0,0 +1,15 @@
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         long SumVal = 0;
+//         for (int I = 1; I < 1000; I++) {
+//             if (I % 3 == 0 || I % 5 == 0) {
+//                 SumVal += I;
+//             }
+//         }
+//         Console.WriteLine("Total=" + SumVal);
+//     }
+// }
---
+++ R05_ずれたインデントをスペース2文字に統一.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+   window.setInterval(function() {
+    var dd = new Date();
+     document.getElementById("T1").innerHTML = dd.toLocaleString();
+  }, 1000);
+}
